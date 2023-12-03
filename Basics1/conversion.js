4c3778ee1b1ee95597066318b9b1792ac26bf978
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2023-12-03 14:36:38 +0000

    Add conversions

---
+++ Basics1/conversion.js
@@ -0,0 +1,19 @@
+let bankBalance = '100'
+let bankBalanceInt = parseInt(bankBalance);
+
+// console.log(typeof bankBalance);
+// console.log(typeof bankBalanceInt);
+
+console.log(bankBalance); //String
+console.log(bankBalanceInt);  //Number
+
+let c = null;
+console.log(c);
+
+console.log(null + undefined)
+console.log(5 + undefined)
+console.log("5" + undefined)
+console.log("" + undefined)
+console.log(null + 5)
+console.log(null + "5")
+console.log(NaN == NaN)
