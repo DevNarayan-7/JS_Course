736b3ef0651b3bb39cb26ade3d8e61518c06270a
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2023-12-03 07:40:43 +0000

    discussed about the data types

---
+++ Basics1/datatypes.js
@@ -0,0 +1,43 @@
+//Two types of datatypes
+//1. Primitive and 2. Reference (Non primitive)
+
+
+/* 
+Primitive data types
+--------------------
+String
+Number 
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+
+--------------------
+const firstName = "Alien"
+const age = 20
+const score = 200.9
+let isLoggedIn = true
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = Symbol("id")
+const aReallyLongNumber = 39393939393933
+
+*/
+
+
+
+/*
+Reference data type
+Array:
+const numbers = [1, 2, 3, 4, 5]
+
+Object:
+const usesNames = {"Alien", "Ravi", "Priya"}
+
+Functions:
+const sayHello = function(){
+    console.log("Alien")
+}
+*/
+
