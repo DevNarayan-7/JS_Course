5db25496f9bef86be66b1d2b4b2d58f2698a0e42
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2023-12-04 04:50:00 +0000

    Methods in String

---
+++ Basics1/numberAndString.js
@@ -0,0 +1,19 @@
+const userName = new String("RaviKumar");
+console.log(userName[0]);
+console.log(userName.length)
+
+let email = "  Ravi@gamil.com      "
+console.log(email.trim())//to trim the spaces 
+
+let emailTwo = "Alien22@gmail.com"
+console.log(emailTwo.replace('22', 'SpaceShip'))// replacing the any character in the string 
+
+let animeName = "Pikachu"
+console.log(animeName.toUpperCase())
+console.log(animeName.toLocaleLowerCase())
+console.log(animeName.charAt(4))
+console.log(animeName[4])
+console.log(animeName.includes('Achu')) // includes method is case-sensitive
+
+
+
