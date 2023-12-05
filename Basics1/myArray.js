fe5e7ac32034357dc01f0b24c5bce2533a870a59
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2023-12-05 05:18:41 +0000

    Discussed on Array

---
+++ Basics1/myArray.js
@@ -0,0 +1,17 @@
+//Arrays
+
+const dcHeroes = ["Superman", "Batman", "Wonder Women","GL", "Flash"]
+//dcHeroes[dcHeroes.length] = "Flash"
+// dcHeroes.push("Flash")
+//let lastElement = dcHeroes.pop();
+
+//dcHeroes.unshift("aquamen");
+// dcHeroes.shift("aquamen");
+// console.log(dcHeroes);
+// console.log(dcHeroes.toString())
+// console.log(dcHeroes.join("-"))
+console.log(dcHeroes)
+console.log(dcHeroes.slice(1,3)) // here it takes 1 and 2 and 3 is not included
+console.log(dcHeroes)
+console.log(dcHeroes.splice(1,3)) // here it takes 1 to 3 and affects the original one and gives the result
+console.log(dcHeroes)
